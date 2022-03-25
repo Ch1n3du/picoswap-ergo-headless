d3ccf4ccb72344555f7250354e9dae49cacde1d6
Ch1n3du <danielonyesoh@gmail.com>
2022-03-25 12:50:42 +0100

    minor stuff

--- src/parsing.rs
+++ src/parsing.rs
@@ -9,16 +9,16 @@ pub fn string_to_constant(stringy: String) -> Constant {
 }
 
 // TODO
-pub fn constant_to_base_16_str(c: Constant) -> &'static str {
+pub fn deserialize_const_to_base_16_str(c: Constant) -> &'static str {
     todo!()
 }
 
 // TODO
-pub fn constant_to_base_58_str(c: Constant) -> &'static str {
+pub fn deserialize_const_to_base_58_str(c: Constant) -> &'static str {
     todo!()
 }
 
 // TODO
-pub fn constant_to_u64(c: Constant) -> u64 {
+pub fn deserialize_constant_to_u64(c: Constant) -> u64 {
     todo!()
 }
