07861d3dfb5d01533f43ecac2fdc0fad14217b8d
Ch1n3du <danielonyesoh@gmail.com>
2022-03-25 12:46:12 +0100

    minor stuff

--- src/parsing.rs
+++ src/parsing.rs
@@ -8,14 +8,17 @@ pub fn string_to_constant(stringy: String) -> Constant {
             .into()
 }
 
+// TODO
 pub fn constant_to_base_16_str(c: Constant) -> &'static str {
     todo!()
 }
 
+// TODO
 pub fn constant_to_base_58_str(c: Constant) -> &'static str {
     todo!()
 }
 
+// TODO
 pub fn constant_to_u64(c: Constant) -> u64 {
     todo!()
 }
